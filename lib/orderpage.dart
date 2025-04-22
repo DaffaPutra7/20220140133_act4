d9e65ec43472f1d75b055710ad10aab62cdcb343
DaffaPutra7 <putradika3604@gmail.com>
2025-04-22 23:04:52 +0700

    Membuat perhitungan untuk total harga dari jumlah makanan dan minuman serta membuat textfield untuk makanan

--- lib/orderpage.dart
+++ lib/orderpage.dart
@@ -8,8 +8,43 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(title: Text('Order Page')),
+      body: Form(
+        child: Column(
+          spacing: 16,
+          children: [
+            TextFormField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: 'Food Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your food order';
+                }
+                return null;
+              },
+            ),
+          ],
+        )
+      ),
+    );
   }
 }
